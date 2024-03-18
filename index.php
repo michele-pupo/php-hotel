1af6d8dee472ca947f0a728986c63122b12172f9
michele-pupo <mikypupo@hotmail.it>
2024-03-18 15:49:02 +0100

    implemented possibility to filter hotels by parking

--- index.php
+++ index.php
@@ -1,44 +1,59 @@
 <?php 
 
-$hotels = [
+    $hotels = [
 
-    [
-        'name' => 'Hotel Belvedere',
-        'description' => 'Hotel Belvedere Descrizione',
-        'parking' => true,
-        'vote' => 4,
-        'distance_to_center' => 10.4
-    ],
-    [
-        'name' => 'Hotel Futuro',
-        'description' => 'Hotel Futuro Descrizione',
-        'parking' => true,
-        'vote' => 2,
-        'distance_to_center' => 2
-    ],
-    [
-        'name' => 'Hotel Rivamare',
-        'description' => 'Hotel Rivamare Descrizione',
-        'parking' => false,
-        'vote' => 1,
-        'distance_to_center' => 1
-    ],
-    [
-        'name' => 'Hotel Bellavista',
-        'description' => 'Hotel Bellavista Descrizione',
-        'parking' => false,
-        'vote' => 5,
-        'distance_to_center' => 5.5
-    ],
-    [
-        'name' => 'Hotel Milano',
-        'description' => 'Hotel Milano Descrizione',
-        'parking' => true,
-        'vote' => 2,
-        'distance_to_center' => 50
-    ],
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
 
-];
+    ];
+
+    $filters_hotel = $hotels;
+
+    //filter con parcheggio
+    if(!empty($_GET['parking'])){
+        $parking = $_GET['parking'] === 'yes' ? true : false;
+
+        $temp_hotels = [];
+        foreach($filters_hotel as $hotel) {
+            if($hotel['parking'] === $parking) {
+                $temp_hotels[] = $hotel;
+            }
+        }
+        $filters_hotel = $temp_hotels;
+    }
 
 ?>
 
@@ -60,16 +75,31 @@ $hotels = [
         <h1 class="display-1 fw-bold d-flex justify-content-center py-4">Hotels</h1>
 
         <!-- form per filtrare gli hotel in base al parcheggio -->
-        <form action="" method="GET">
-            <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="parkingFilter" name="parkingFilter" value="si">
-                <label class="form-check-label" for="parkingFilter">Hotel con parcheggio</label>
+        <form action="index.php" method="GET">
+            <div class="row g-3 pb-3 align-items-center d-flex justify-content-center">
+                <!-- checkbox per selezionare "con parcheggio" -->
+                <div class="col-auto">
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="parking" id="parkingYes" value="yes" <?php if (!empty($_GET['parking']) && $_GET['parking'] === 'yes') echo 'checked'; ?>>
+                        <label class="form-check-label" for="parkingYes">Con parcheggio</label>
+                    </div>
+                </div>
+                <!-- checkbox per selezionare "senza parcheggio" -->
+                <div class="col-auto">
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="parking" id="parkingNo" value="no" <?php if (!empty($_GET['parking']) && $_GET['parking'] === 'no') echo 'checked'; ?>>
+                        <label class="form-check-label" for="parkingNo">Senza parcheggio</label>
+                    </div>
+                </div>
+                <!-- bottone di submit -->
+                <div class="col-auto">
+                    <button type="submit" class="btn btn-primary">Cerca</button>
+                </div>
             </div>
-            <button type="submit" class="btn btn-primary">Cerca</button>
         </form>
         <!-- fine form per filtrare gli hotel in base al parcheggio -->
     
-        <!-- stampa dati hotel in tabella -->
+        <!-- stampa dati di tutti gli hotel in tabella -->
         <table class="table text-center">
             <thead>
                 <tr>
@@ -95,7 +125,36 @@ $hotels = [
             <?php endforeach; ?>
             </tbody>
         </table>
-        <!-- fine stampa dati hotel in tabella -->
+        <!-- fine stampa dati di tutti gli hotel in tabella -->
+        
+        <h2 class="display-1 fw-bold d-flex justify-content-center py-4">Hotels filtrati</h2>
+
+        <!--stampa dati degli hotel filtrati in tabella-->
+        <table class="table text-center">
+            <thead>
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Parking</th>
+                    <th scope="col">Vote</th>
+                    <th scope="col">Distance_to_center</th>
+                </tr>
+            </thead>
+            <tbody>
+            <!-- ciclo foreach per ciclare sugli hotel filtrati -->
+            <?php foreach ($filters_hotel as $hotel) :?>
+                <tr>
+                    <td><?php echo $hotel['name']; ?></th>
+                    <td><?php echo $hotel['description']; ?></td>
+                    <!-- trasformiamo il "tru o false" del parcheggio in "si o no" -->
+                    <td><?php echo $hotel['parking'] ? 'Si' : 'No'; ?></td>
+                    <td><?php echo $hotel['vote']; ?></td>
+                    <td><?php echo $hotel ['distance_to_center']; ?>km</td>
+                </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+        <!--fine stampa dati degli hotel filtrati in tabella-->
     </div>
 
 <!-- script bootstrap -->
